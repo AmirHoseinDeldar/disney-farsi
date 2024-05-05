f63e40ba4c1df0a8b98b62804ba1e7a9929a36ae
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-05-05 15:17:23 +0330

    header Done

--- components/GenreDropdown.tsx
+++ components/GenreDropdown.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Genres } from "@/typings";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
 
-const GenreDropdown = () => {
+async function GenreDropdown() {
+  const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+    next: {
+      revalidate: 60 * 60 * 24, // 24H Caching 1milion request = 1
+    },
+  };
+
+  const response = await fetch(url.toString(), options);
+  const data = (await response.json()) as Genres;
+
   return (
-    <div>GenreDropdown</div>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger className="text-white flex justify-center items-center">
+        ژانر
+        <ChevronDown className="ml-1" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>انتخاب ژانر</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {data.genres.map((genre) => (
+          <DropdownMenuItem className="cursor-pointer" key={genre.id}>
+            <Link href={`/genre/${genre.id}?genre=${genre.name}`}>
+              {genre.name}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
-export default GenreDropdown
+export default GenreDropdown;
